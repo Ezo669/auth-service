d80c5fdfb196e16d8eaf4951ca04a7a70aa20ef1
kairis <kairi@darasa.io>
2025-01-21 12:49:42 +0700

    kairis

--- src/main/java/com/authen/controller/LoginController.java
+++ src/main/java/com/authen/controller/LoginController.java
@@ -2,15 +2,19 @@ package com.authen.controller;
 
 import com.authen.model.AuthorizationResponse;
 import com.authen.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/api")
+@Slf4j
 public class LoginController {
 
     @Autowired
